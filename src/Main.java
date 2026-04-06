b1734b73541258ad83a4c955a3affa68c64c23cc
EduardoLC17 <eduardo.lopezcozar@gmail.com>
2026-04-06 12:45:18 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,40 @@
 void main() {
 
 
+
+    String menu = """
+                        Introduce la opcion deseada
+                        1--Valor absoluto
+                        2--Raiz cuadrada
+                        0--Cerrar
+    
+                        """;
+    System.out.println(menu);
+    Scanner sc = new Scanner(System.in);
+
+    int opcion = sc.nextInt();
+
+
+    do {
+        System.out.println(menu);
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1 -> {
+                System.out.println("Introduce el numero para el calculo");
+                double numero1 = sc.nextDouble();
+                System.out.println(miniCalculadoraEjemplo.valorAbsoluto(numero1));
+            }
+            case 2 -> {
+                System.out.println("Introduce el numero para el calculo");
+                double numero1 = sc.nextDouble();
+                System.out.println(miniCalculadoraEjemplo.raizCuadrada(numero1));
+            }
+            case 0 -> System.out.println("Cerrando");
+        }
+    } while (opcion != 0);
+    sc.close();
+
+
+
 }
