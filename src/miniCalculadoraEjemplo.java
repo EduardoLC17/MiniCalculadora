0ef787031da7203326cd7f08129766fcdf50b193
EduardoLC17 <eduardo.lopezcozar@gmail.com>
2026-04-09 11:57:46 +0200

    fid.raiz

--- src/miniCalculadoraEjemplo.java
+++ src/miniCalculadoraEjemplo.java
@@ -1,6 +1,16 @@
 public class miniCalculadoraEjemplo {
-
+    /**
+     * Calcula la raíz cuadrada de un número.
+     *
+     * @author Eduardo
+     * @version 1.0
+     * @since 1.0
+     * @param numero El número del que se quiere obtener la raíz cuadrada.
+     * @return La raíz cuadrada del número proporcionado.
+     * @see Math#sqrt(double)
+     */
     public static double raizCuadrada(double numero) {
+
         return Math.sqrt(numero);
     }
 
