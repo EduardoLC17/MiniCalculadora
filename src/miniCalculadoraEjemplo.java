ff172930de984745e810e4e1d7062cd486c8300f
EduardoLC17 <eduardo.lopezcozar@gmail.com>
2026-04-09 11:59:13 +0200

    d.absoluto

--- src/miniCalculadoraEjemplo.java
+++ src/miniCalculadoraEjemplo.java
@@ -14,6 +14,16 @@ public class miniCalculadoraEjemplo {
         return Math.sqrt(numero);
     }
 
+    /**
+     * Calcula el valor absoluto de un número.
+     *
+     * @author Eduardo
+     * @version 1.0
+     * @since 1.0
+     * @param numero El número del que se quiere obtener el valor absoluto.
+     * @return El valor absoluto del número proporcionado.
+     * @see Math#abs(double)
+     */
     public static double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
